36561d186ef15d97aa27ad1a13a2eba631e01f4a
nimaessi <nimaesfahani92@gmail.com>
2023-11-18 02:01:06 +0330

    basic info form finish :wink:

--- src/components/resume/basic-info/FormInfo.jsx
+++ src/components/resume/basic-info/FormInfo.jsx
@@ -2,25 +2,25 @@ import { Col, Row } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import iranProvinces from "../../../utils/provinces.js";
 import iranCities from "../../../utils/cities.js";
-import { useDispatch, useSelector } from "react-redux";
-import { selectInformation, setBasicInfo } from "../../../features/information/informationSlice.js";
+import { useDispatch } from "react-redux";
+import { setBasicInfo } from "../../../features/information/informationSlice.js";
 const FormInfo = () => {
 
-  const information = useSelector(selectInformation);
   const dispatch = useDispatch();
-
-  const handleChange  = (event) => {
-    dispatch(setBasicInfo({ [event.target.name]: event.target.value} ));
+  const handleChange = (event) => {
+    dispatch(setBasicInfo({field : event.target.name, value: event.target.value }));
   }
+
   return (  
     <Row>
         <Col md = {7}>
           <Form>
-            <Form.Group className="mb-3" controlId="resName">
+            <Form.Group className="mb-3" controlId="resumeName">
                 <Form.Label className = "text-muted p-2">نام رزومه</Form.Label>
                 <Form.Control
                   type = "text"
-                  name = "resName"
+                  name = "resumeName"
+                  maxLength = {20}
                   onChange = {handleChange}
                   placeholder="برای مثال : رزومه معماری" />
             </Form.Group>
@@ -33,6 +33,7 @@ const FormInfo = () => {
                   <Form.Control 
                     type="text"
                     name = "firstName"
+                    maxLength = {40}
                     onChange = {handleChange} 
                     placeholder = "نام" />
                 </Form.Group>
@@ -41,7 +42,12 @@ const FormInfo = () => {
                     نام خانوادگی
                     <span className = "text-danger fw-bold fs-3"> * </span>
                   </Form.Label>
-                  <Form.Control type="text" placeholder="نام خانوادگی" />
+                  <Form.Control 
+                    type="text"
+                    name = "lastName"
+                    onChange = {handleChange}
+                    maxLength = {80}
+                    placeholder="نام خانوادگی" />
                 </Form.Group>
               </Row>
               <Row className="mb-3">
@@ -50,13 +56,22 @@ const FormInfo = () => {
                     شماره همراه
                     <span className = "text-danger fw-bold fs-3"> * </span>
                   </Form.Label>
-                  <Form.Control type="text" placeholder = "شماره همراه" />
+                  <Form.Control 
+                    type = "tel"
+                    name = "mobile"
+                    maxLength = {70}
+                    onChange = {handleChange}
+                    placeholder = "شماره همراه" />
                 </Form.Group>
                 <Form.Group as = {Col} controlId = "gender">
                   <Form.Label className = "text-muted ms-2">جنسیت
                     <span className = "text-danger fw-bold fs-3"> * </span>
                   </Form.Label>
-                  <Form.Select className = "text-muted"  defaultValue = "none">
+                  <Form.Select 
+                    name = "gender"
+                    onChange = {handleChange} 
+                    className = "text-muted"  
+                    defaultValue = "none">
                     <option className = "text-muted" value = "none">جنسیت</option>
                     <option className = "text-muted" value = "men">مرد</option>
                     <option className = "text-muted" value = "women">زن</option>
@@ -68,19 +83,27 @@ const FormInfo = () => {
                   <Form.Label className = "text-muted">
                     نظام وظیفه
                   </Form.Label>
-                  <Form.Select className = "text-muted" defaultValue = "none">
+                  <Form.Select
+                    name = "militaryStatus"
+                    onChange = {handleChange} 
+                    className = "text-muted" 
+                    defaultValue = "none">
                     <option className = "text-muted" value = "none">وضعیت نظام وظیفه</option>
-                    <option className = "text-muted" value = "men">معافیت تحصیلی</option>
-                    <option className = "text-muted" value = "none">معافیت</option>
-                    <option className = "text-muted" value = "men">مشمول</option>
-                    <option className = "text-muted" value = "women">پایان خدمت</option>
+                    <option className = "text-muted" value = "educationPardon">معافیت تحصیلی</option>
+                    <option className = "text-muted" value = "exemption">معافیت</option>
+                    <option className = "text-muted" value = "included">مشمول</option>
+                    <option className = "text-muted" value = "end">پایان خدمت</option>
                   </Form.Select>
                 </Form.Group>
-                <Form.Group as = {Col} controlId = "Military_Status">
+                <Form.Group as = {Col} controlId = "maritalStatus">
                   <Form.Label className = "text-muted">
                     وضعیت تاهل
                   </Form.Label>
-                  <Form.Select className = "text-muted" defaultValue = "none">
+                  <Form.Select
+                    name = "maritalStatus"
+                    onChange = {handleChange} 
+                    className = "text-muted" 
+                    defaultValue = "none">
                     <option className = "text-muted" value = "none">وضعیت تاهل</option>
                     <option className = "text-muted" value = "single">مجرد</option>
                     <option className = "text-muted" value = "married">متاهل</option>
@@ -92,12 +115,21 @@ const FormInfo = () => {
                   <Form.Label className = "text-muted">
                     استان
                   </Form.Label>
-                  <Form.Select className = "text-muted" defaultValue = "none">
+                  <Form.Select
+                    name = "province"
+                    onChange = {handleChange}
+                    className = "text-muted" 
+                    defaultValue = "none">
                     <option className = "text-muted" value = {0}>استان خود را انتخاب کنید</option>
                     {
                       iranProvinces.map((iranProvince) => {
                         return(
-                          <option key = {iranProvince.id} className = "text-muted" value = {iranProvince.id}>{iranProvince.name}</option>
+                          <option 
+                            key = {iranProvince.id} 
+                            className = "text-muted" 
+                            value = {iranProvince.id}>
+                              {iranProvince.name}
+                          </option>
                         )
                       })
                     }
@@ -107,7 +139,11 @@ const FormInfo = () => {
                   <Form.Label className = "text-muted">
                     شهر
                   </Form.Label>
-                  <Form.Select className = "text-muted" defaultValue = "none">
+                  <Form.Select
+                    name = "city"
+                    onChange = {handleChange}
+                    className = "text-muted" 
+                    defaultValue = "none">
                     <option className = "text-muted" value = {0}>شهر خود را انتخاب کنید</option>
                     {
                       iranCities.map((iranCities) => {
@@ -126,7 +162,11 @@ const FormInfo = () => {
               </Row>
               <Form.Group className="mb-3" controlId="Address">
                 <Form.Label className = "text-muted">آدرس</Form.Label>
-                <Form.Control />
+                <Form.Control
+                  name = "address"
+                  type = "text"
+                  maxLength = {120}
+                  onChange = {handleChange}/>
               </Form.Group>
           </Form>
 
--- src/features/information/informationSlice.js
+++ src/features/information/informationSlice.js
@@ -1,33 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    basicInformation: [],
-    education: [],
-    workExperince: [],
-    skills: [],
-    furtherInformation: []
+    resumeName: "",
+    firstName: "",
+    lastName: "",
+    mobile: "",
+    gender: "",
+    militaryStatus: "",
+    maritalStatus: "",
+    province: "",
+    city: "",
+    address: ""
+   
 }
 const informationSlice = createSlice({
     name: "information",
     initialState,
     reducers:{
         setBasicInfo: (state, action) => {
-            state.basicInformation.push(action.payload);
+            state[action.payload.field] = action.payload.value;
         },
-        setEducation: (state, action) => {
-            state.education = action.payload;
-        },
-        setWorkExperience: (state, action) => {
-            state.workExperince = action.payload;
-        },
-        setSkills: (state, action) => {
-            state.skills = action.payload;
-        },
-        setFurtherInformation: (state, action) => {
-            state.furtherInformation = action.payload;
+        resetInfo: state => {
+            state.resumeName = "",
+            state.firstName = "",
+            state.lastName = "",
+            state.mobile = "",
+            state.gender = "",
+            state.militaryStatus = "",
+            state.maritalStatus = "",
+            state.province = "",
+            state.city = "",
+            state.address = ""
+
         }
     }
 });
 export default informationSlice.reducer;
-export const { setBasicInfo, setEducation, setWorkExperience, setSkills, setFurtherInformation} = informationSlice.actions;
+export const { setBasicInfo, resetInfo} = informationSlice.actions;
 export const selectInformation = (store) => store.information;
