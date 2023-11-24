075ed2779a4295fded049c1cc7d39b6019d295e8
nimaessi <nimaesfahani92@gmail.com>
2023-11-24 17:32:35 +0330

    add steps menu :wink:

--- src/components/resume/steps/Steps.jsx
+++ src/components/resume/steps/Steps.jsx
@@ -1,25 +1,41 @@
 import { Link } from "react-router-dom";
 import Resume from "../Resume";
 import ListGroup from 'react-bootstrap/ListGroup';
+import * as Icon from "react-bootstrap-icons";
 
 const Steps = () => {
   return (
     <Resume displayBtn = {false} >
         <ListGroup>
             <ListGroup.Item className = "border border-0 p-2 m-2">
-                <Link className = "nav-link" to = "/resume/basic-info">اطلاعات پایه</Link>
+                <Link className = "nav-link text-muted fs-5" to = "/resume/basic-info">
+                    <Icon.InfoCircleFill className = "me-2" />
+                    اطلاعات پایه
+                </Link>
             </ListGroup.Item>
             <ListGroup.Item className = "border border-0 p-2 m-2">
-                <Link className = "nav-link" to = "/resume/basic-info"> تحصیلات</Link>
+                <Link className = "nav-link text-muted fs-5" to = "/resume/basic-info">
+                    <Icon.BookFill className = "me-2" />
+                     تحصیلات
+                </Link>
             </ListGroup.Item>
             <ListGroup.Item className = "border border-0 p-2 m-2">
-                <Link className = "nav-link" to = "/resume/basic-info"> سوابق شغلی</Link>
+                <Link className = "nav-link text-muted fs-5" to = "/resume/basic-info">
+                    <Icon.BriefcaseFill className = "me-2" />
+                    سوابق شغلی  
+                </Link>
             </ListGroup.Item>
             <ListGroup.Item className = "border border-0 p-2 m-2">
-                <Link className = "nav-link" to = "/resume/basic-info">مهارت ها</Link>
+                <Link className = "nav-link text-muted fs-5" to = "/resume/basic-info">
+                    <Icon.Wrench className = "me-2" />
+                    مهارت ها    
+                </Link>
             </ListGroup.Item>
             <ListGroup.Item className = "border border-0 p-2 m-2">
-                <Link className = "nav-link" to = "/resume/basic-info"> اطلاعات تکمیلی</Link>  
+                <Link className = "nav-link text-muted fs-5" to = "/resume/basic-info">
+                    <Icon.PlusCircle className = "me-2" />
+                     اطلاعات تکمیلی
+                </Link>  
             </ListGroup.Item>
         </ListGroup>
     </Resume>
