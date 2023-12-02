ca6cd5750afadc2a112d266f5811d6fab5bae65b
nimaessi <nimaesfahani92@gmail.com>
2023-12-02 14:25:09 +0330

    working edu form 🚧

--- src/components/resume/education/Education.jsx
+++ src/components/resume/education/Education.jsx
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
-
+import grade from "../../../utils/grade";
 const Education = () => {
   return (
     <Resume>
@@ -14,25 +14,54 @@ const Education = () => {
             <Row className = "mb-3">
               <Form.Group as = {Col}>
                 <Form.Label className = "text-muted p-2"> مقطع تحصیلی</Form.Label>
-                <Form.Control type = "text" />
+                <Form.Select 
+                  name = "grade"
+                  defaultValue = {0}>
+                  <option className = "text-muted" value = {0}>مقطع تحصیلی خود را انتخاب کنید.</option>
+                  {
+                    grade.map(item => {
+                      return(
+                        <option
+                          key = {item.id} 
+                          className = "text-muted" 
+                          value = {item.id}>
+                          {item.label}
+                        </option>
+                      )
+                    })
+                  }
+                </Form.Select>
               </Form.Group>
               <Form.Group as = {Col}>
                 <Form.Label className = "text-muted p-2"> رشته تحصیلی</Form.Label>
-                <Form.Control type = "text" />
+                <Form.Control 
+                  type = "text"
+                  name = "field"
+                  maxLength = {30} />
               </Form.Group>
             </Row>
             <Form.Group className = "mb-3">
               <Form.Label className = "text-muted p-2">نام دانشگاه</Form.Label>
-              <Form.Control type = "text"/>
+              <Form.Control 
+                type = "text"
+                name = "university"
+                maxLength = {30}/>
             </Form.Group>
             <Row className = "mb-3">
               <Form.Group as = {Col}>
                 <Form.Label className = "text-muted p-2">گرایش</Form.Label>
-                <Form.Control type = "text" />
+                <Form.Control 
+                  type = "text"
+                  name = "academicOrientation"
+                  maxLength = {30} />
               </Form.Group>
               <Form.Group as = {Col}>
                 <Form.Label className = "text-muted p-2">معدل</Form.Label>
-                <Form.Control type = "text" />
+                <Form.Control 
+                  type = "number"
+                  min = {0}
+                  max = {20}
+                  name = "average"/>
               </Form.Group>
             </Row>
             <Row className = "mb-3 align-items-center">
@@ -40,6 +69,7 @@ const Education = () => {
                 <Form.Label className = "text-muted p-2"> تاریخ شروع </Form.Label>
                 <DatePicker
                     style = {{height : '35px'}}
+                    name = "start"
                     calendar = {persian}
                     locale = {persian_fa}
                     calendarPosition = "bottom-right"/>
@@ -48,12 +78,17 @@ const Education = () => {
                 <Form.Label className = "text-muted p-2">تاریخ پایان</Form.Label>
                 <DatePicker
                     style = {{height : '35px'}}
+                    name = "end"
                     calendar = {persian}
                     locale = {persian_fa}
                     calendarPosition = "bottom-right"/>
               </Form.Group>
               <Form.Group as = {Col} className = "mt-5">
-                <Form.Check className = "text-muted" type="checkbox" label="تاکنون" />
+                <Form.Check 
+                  className = "text-muted" 
+                  type="checkbox"
+                  name = "studying" 
+                  label="تاکنون" />
               </Form.Group>
             </Row>
           </Form>
---
+++ src/utils/grade.js
@@ -0,0 +1,28 @@
+const grade = [
+    {
+        id: 1,
+        name: "diploma",
+        label: "دیپلم"
+    },
+    {
+        id: 2,
+        name: "associate",
+        label: "کاردانی"
+    },
+    {
+        id: 3,
+        name: "bachelor",
+        label: "کارشناسی"
+    },
+    {
+        id: 4,
+        name: "master",
+        label: "کارشناسی ارشد"
+    },
+    {
+        id: 5,
+        name: "doctorate",
+        label: "دکترا"
+    }
+];
+export default grade;
