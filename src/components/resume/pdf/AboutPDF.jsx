c6f571afe5750108abeaa1fbcee0e71933d705a7
nimaessi <nimaesfahani92@gmail.com>
2024-02-10 11:32:41 +0330

    work on pdf generator 🚧

--- src/components/resume/pdf/AboutPDF.jsx
+++ src/components/resume/pdf/AboutPDF.jsx
@@ -1,14 +1,16 @@
 import * as Icon from "react-bootstrap-icons";
+import { useSelector } from "react-redux";
+import { selectInformation } from "../../../features/information/informationSlice";
 
 const AboutPDF = () => {
+
+  const info = useSelector(selectInformation);
   return (
     <>
         <h6 className = "pb-2 text-muted ms-3 border-3 border-bottom border-success border-opacity-75">
             <Icon.PersonCircle className = "text-success me-2" />درباره من
         </h6>
-        <p className = "text-muted lh-base justify-text">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقیه قرار گیرد.
-        </p>
+        <p className = "text-muted lh-base justify-text">{info.additionalInformation}</p>
     </>
   )
 }
