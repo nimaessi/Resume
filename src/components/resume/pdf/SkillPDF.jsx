f8c5abe37118c390d2b76a762e8888d1087ec8a7
nimaessi <nimaesfahani92@gmail.com>
2024-02-20 02:41:11 +0330

    beta version pdf generator finish :wink:

--- src/components/resume/pdf/SkillPDF.jsx
+++ src/components/resume/pdf/SkillPDF.jsx
@@ -1,6 +1,10 @@
 import * as Icon from "react-bootstrap-icons";
 import { Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { selectSkill } from "../../../features/skills/skillSlice";
 const SkillPDF = () => {
+    
+    const skills = useSelector(selectSkill);
   return (
     <>
     <h6 className = "mt-5 pb-2 text-muted ms-3 border-3 border-bottom border-success border-opacity-75 w-75">
@@ -8,116 +12,38 @@ const SkillPDF = () => {
     </h6>
         <Row>
             <Col md = {6}>
-                <p className = "text-muted ms-2 mt-3">
-                    نام مهارت
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    
-                </p>
-                <p className = "text-muted ms-2 mt-3">
-                    نام مهارت
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    
-                </p>
-                <p className = "text-muted ms-2 mt-3">
-                    نام مهارت
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    
-                </p>
-                <p className = "text-muted ms-2 mt-3">
-                    نام مهارت
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    
-                </p>
-                <p className = "text-muted ms-2 mt-3">
-                    نام مهارت
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    
-                </p>
-                <p className = "text-muted ms-2 mt-3">
-                    نام مهارت
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-warning ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    <Icon.StarFill className = "text-muted ms-2" />
-                    
-                </p>
+                {skills.skill.map((item,index) => {
+                    if(index % 2 == 0){
+                        return(
+                            <p className = "text-muted ms-2 mt-3" key = {item.id}>
+                            {item.skill}
+                            { 
+                                [...new Array(4)].map((arr,index) => {
+                                    return index < item.rate ? <Icon.StarFill className = "text-warning ms-2" /> 
+                                    : <Icon.StarFill className = "text-muted ms-2" />
+                                })
+                            }
+                            </p>
+                        )
+                    }
+                })}
             </Col>
             <Col md = {6}>
-            <p className = "text-muted ms-2 mt-3">
-                نام مهارت
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                
-            </p>
-            <p className = "text-muted ms-2 mt-3">
-                نام مهارت
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                
-            </p>
-            <p className = "text-muted ms-2 mt-3">
-                نام مهارت
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                
-            </p>
-            <p className = "text-muted ms-2 mt-3">
-                نام مهارت
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                
-            </p>
-            <p className = "text-muted ms-2 mt-3">
-                نام مهارت
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                
-            </p>
-            <p className = "text-muted ms-2 mt-3">
-                نام مهارت
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-warning ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                <Icon.StarFill className = "text-muted ms-2" />
-                
-            </p>
+                {skills.skill.map((item,index) => {
+                    if(index % 2 != 0){
+                        return(
+                            <p className = "text-muted ms-2 mt-3" key = {item.id}>
+                            {item.skill}
+                            { 
+                                [...new Array(4)].map((arr,index) => {
+                                    return index < item.rate ? <Icon.StarFill className = "text-warning ms-2" /> 
+                                    : <Icon.StarFill className = "text-muted ms-2" />
+                                })
+                            }
+                            </p>
+                        )
+                    }
+                })}
             </Col>
         </Row>
     </>
